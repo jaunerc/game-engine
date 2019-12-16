5c92aa922c7b376575845e97d610791f01aa7797
Cyrill Jauner <c.jauner@gmail.com>
2019-12-16 21:08:45 +0100

    implement functionality to the body class

--- physics/src/main/java/ch/travbit/game_engine/physics/Body.java
+++ physics/src/main/java/ch/travbit/game_engine/physics/Body.java
@@ -3,25 +3,112 @@ package ch.travbit.game_engine.physics;
 import ch.travbit.game_engine.physics.shapes.Shape;
 import org.joml.Vector2f;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * This class represents a physical body.
+ * <p>
+ * A body holds information about the position in the physical world. It can detect collisions with other bodies and
+ * notify observers when a collision occurs. A body can have a shape to define its collision boundaries.
+ */
 public class Body {
 
     private Vector2f position;
     private Shape shape;
 
+    private List<CollisionObserver> collisionObservers;
+
     public Body() {
-        position = new Vector2f(0f,0f);
+        this(0, 0, null);
     }
 
+    public Body(float x, float y, Shape shape) {
+        this(new Vector2f(x, y), shape);
+    }
+
+    public Body(Vector2f position, Shape shape) {
+        this.position = position;
+        this.shape = shape;
+        collisionObservers = new ArrayList<>();
+        setPosition(position);
+    }
+
+    /**
+     * Translates this body by the given translation vector.
+     *
+     * @param translation in cartesian coordinates
+     */
     public void translate(Vector2f translation) {
         position.add(translation);
-        shape.translate(translation);
+        getShape().ifPresent(shape -> shape.translate(translation));
+    }
+
+    /**
+     * Translates this body by the given cartesian coordinates.
+     *
+     * @param x translation on the x-axis
+     * @param y translation on the y-axis
+     */
+    public void translate(float x, float y) {
+        translate(new Vector2f(x, y));
+    }
+
+    /**
+     * Sets the position of this body directly to the given position vector.
+     *
+     * @param position the new position of the body
+     */
+    public void setPosition(Vector2f position) {
+        this.position = position;
+        getShape().ifPresent(shape -> shape.setShapeAtPosition(position));
+    }
+
+    /**
+     * Sets the position of this body directly by the given cartesian coordinates.
+     *
+     * @param x position on the x-axis
+     * @param y position on the y-axis
+     */
+    public void setPosition(float x, float y) {
+        setPosition(new Vector2f(x, y));
     }
 
     public Vector2f getPosition() {
-        return position;
+        return new Vector2f(position);
     }
 
-    public Shape getShape() {
-        return shape;
+    public Optional<Shape> getShape() {
+        return Optional.ofNullable(shape);
+    }
+
+    /**
+     * Add the given object to the list of observers that are notified about a collision.
+     *
+     * @param observer the observer object
+     */
+    public void addCollisionObserver(CollisionObserver observer) {
+        collisionObservers.add(observer);
+    }
+
+    /**
+     * Whether this body has collided with the other body.
+     *
+     * @param other the other body to check for collision
+     * @return true if this body collided with the other body; false otherwise
+     */
+    public boolean isCollidedWith(Body other) {
+        boolean isCollided = false;
+
+        if (other.getShape().isPresent() && getShape().isPresent()) {
+            isCollided = getShape().get().intersectsWith(other.getShape().get());
+        }
+
+        if (isCollided) {
+            collisionObservers.forEach(CollisionObserver::reactOnCollision);
+        }
+
+        return isCollided;
     }
 }
---
+++ physics/src/main/java/ch/travbit/game_engine/physics/CollisionObserver.java
@@ -0,0 +1,6 @@
+package ch.travbit.game_engine.physics;
+
+public interface CollisionObserver {
+
+    void reactOnCollision();
+}
