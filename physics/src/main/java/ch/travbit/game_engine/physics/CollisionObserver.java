e27b2fe8e12fc88542e1078f6a3930ea037e4b5b
Cyrill Jauner <c.jauner@gmail.com>
2019-12-20 15:16:09 +0100

    add comments

--- physics/src/main/java/ch/travbit/game_engine/physics/CollisionObserver.java
+++ physics/src/main/java/ch/travbit/game_engine/physics/CollisionObserver.java
@@ -1,6 +1,16 @@
 package ch.travbit.game_engine.physics;
 
+/**
+ * This interface provides a method to react on a collision.
+ * <p>
+ * It acts as an observer in the body class.
+ */
 public interface CollisionObserver {
 
+    /**
+     * React on a collision.
+     * <p>
+     * A collision has occurred and the observer is notified.
+     */
     void reactOnCollision();
 }
